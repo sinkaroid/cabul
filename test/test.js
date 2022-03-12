85a21408b26c2a989f1ff572c2069e4e8457c73a
d0g <bibsindra@gmail.com>
2022-03-12 16:27:42 +0700

    test: update testing

--- test/test.js
+++ test/test.js
@@ -3,15 +3,15 @@ const reddit = new Cabul()
 
 // To use and import hentai only
 reddit.hentai("ahegao", "hot").then(res => {
-    console.log(res); // Returns a object
+    console.log(res);
 });
 
 // To use and import irl only
 reddit.irl("gonewild", "top").then(res => {
-    console.log(res); // Returns a object
+    console.log(res);
 });
 
 // To use and import memes only
 reddit.meme("hentaimemes", "controversial").then(res => {
-    console.log(res); // Returns a object
+    console.log(res);
 });
